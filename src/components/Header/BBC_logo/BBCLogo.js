b3eabe1e5a5b5f5bf4c7c150bb1663511c75cd36
ecleaver <erincleaver@grcc.email.edu>
2021-07-28 19:09:34 -0400

    Added header

---
+++ src/components/Header/BBC_logo/BBCLogo.js
@@ -0,0 +1,9 @@
+const BBCLogo = () => {
+    return (
+        <div className="bbc_logo">
+            <p><span>B</span>B<span>C</span></p>
+        </div>
+    )    
+}
+
+export default BBCLogo
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './header.css';
+
+const Header = () => {
+    return (
+        <header>
+            
+        </header>
+    )
+}
+
+export default Header
+
+
